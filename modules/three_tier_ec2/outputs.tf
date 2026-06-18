cc5d36074b75e6fcacfd83e8d0314d433e076505
Xabiere-Designs <xabieredesigns@gmail.com>
2026-06-18 09:20:59 -0400

    Created monitoring module folder

--- modules/three_tier_ec2/outputs.tf
+++ modules/three_tier_ec2/outputs.tf
@@ -33,3 +33,15 @@ output "web2_security_group_id" {
   description = "Security group ID attached to web2"
   value       = aws_security_group.web2_sg.id
 }
+
+# Security group ID for monitoring server, used to allow access from web1/bastion
+output "web1_security_group_id" {
+  description = "Security group ID for web1 bastion server"
+  value       = aws_security_group.web1_sg.id
+}
+
+# Security group ID for monitoring server, used to allow access from web1/bastion
+output "web2_security_group_id" {
+  description = "Security group ID for web2 application server"
+  value       = aws_security_group.web2_sg.id
+}
